7848da37029923f957df6a1b59cbc4078b990fe3
timohus <timohus@gmail.com>
2013-08-31 17:26:04 +0300

    update

---
+++ engine/Engine.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This is a Main class wich contains all the Controller's functionality
+ */
+
+class Engine {
+    public function __construct() {
+        echo "Welcome to website engine";
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-    
+    include_once $_SERVER['DOCUMENT_ROOT']."/engine/Engine.php";
+    //Starting the engine 
+    $engine = new Engine();
 ?>
